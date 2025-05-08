be2ea9822fccf9362a6cf3d8f213a3192a6caa49
Steve Jenson <stevej@gmail.com>
2025-05-07 18:20:48 -0700

    kill dead comments in test.py

--- test/test.py
+++ test/test.py
@@ -25,20 +25,12 @@ async def test_project(dut):
 
     dut._log.info("Test project behavior")
 
-    # Set the input values you want to test
-    #dut.ui_in.value = 20
-    #dut.uio_in.value = 30
-
     # Wait for one clock cycle to see the output values
     await ClockCycles(dut.clk, 1)
-
-    # The following assersion is just an example of how to check the output values.
-    # Change it to match the actual expected output of your module:
 
     numbers = []
     for i in range(20):
         number = int(dut.uo_out.value)
-        #print(number)
         numbers.append(number)
         await ClockCycles(dut.clk, 1)
 
@@ -47,8 +39,6 @@ async def test_project(dut):
     # Few duplicates were found
     assert len(unique_numbers) > 17
 
-    # Keep testing the module by changing the input values, waiting for
-    # one or more clock cycles, and asserting the expected output values.
 
 
 
@@ -71,15 +61,8 @@ async def test_seed(dut):
 
     dut._log.info("Test project behavior")
 
-    # Set the input values you want to test
-    #dut.ui_in.value = 20
-    #dut.uio_in.value = 30
-
     # Wait for one clock cycle to see the output values
     await ClockCycles(dut.clk, 1)
-
-    # The following assersion is just an example of how to check the output values.
-    # Change it to match the actual expected output of your module:
 
     dut.ui_in.value = 0x50
     dut.uio_in.value = 0b1000_0000
